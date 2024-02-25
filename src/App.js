0ca4b362fb56708a62ed20d8f35a2e9e2e8b35b0
Rashika <60001683+rashika10@users.noreply.github.com>
2024-02-25 20:47:37 +0545

    day07 going back to normal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
  function App() {
 
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
@@ -41,17 +41,16 @@ import React, { useState } from 'react';
   }
   return (
     <>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
     <Navbar title="TextUtilss" aboutText="About" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3" >
-    {/* <Routes> */}
-        {/* <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter Your Heading Below" mode={mode}/>} /> */}
-        {/* <Route path="about/" element={<About />} /> */}
-      {/* </Routes> */}
-      <TextForm showAlert={showAlert} heading="Enter Your Heading Below" mode={mode}/>
+    <Routes>
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter Your Heading Below" mode={mode}/>} /> 
+        <Route path="about/" element={<About />} />
+    </Routes>
     </div>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
     </>
   );
 }
