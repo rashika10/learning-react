68496b0845c10022ca75f99f40280b107d91d1d9
Rashika <60001683+rashika10@users.noreply.github.com>
2024-02-28 20:49:08 +0545

    day09 many small changes are done

--- src/App.js
+++ src/App.js
@@ -29,13 +29,13 @@ import {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled","success");
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled","success")
-      document.title = 'TextUtils - Light Mode';
+      // document.title = 'TextUtils - Light Mode';
       // check setInterval  to make title shine
     }
   }
@@ -46,7 +46,7 @@ import {
     <Alert alert={alert}/>
     <div className="container my-3" >
     <Routes>
-        <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter Your Heading Below" mode={mode}/>} /> 
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="TextUtils - Word Counter, Character Counter, Remove Extra Space" mode={mode}/>} /> 
         <Route path="about/" element={<About  mode={mode}/>} />
     </Routes>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,7 +36,8 @@ export default function TextForm(props) {
   const handleCopy = () =>{
     let text = document.getElementById("myBox");
     text.select();
-    navigator.clipboard.writeText(text.value)
+    navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Text Copied!","success");
   }
 
@@ -52,24 +53,24 @@ export default function TextForm(props) {
   return (
     <>
     <div className="container" style={{color: props.mode ==='dark'?'white':'#042743'}}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-3'>{props.heading}</h1>
         <div className="mb-3"> 
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode ==='dark'?'grey':'white', color: props.mode ==='dark'?'white':'#042743'}} id="myBox" rows="7"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode ==='dark'?'#13466e':'white', color: props.mode ==='dark'?'white':'#042743'}} id="myBox" rows="7"></textarea>
         </div>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleTitleClick}>Convert to Title Case</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpace}>Remove Extra Space</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleTitleClick}>Convert to Title Case</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleExtraSpace}>Remove Extra Space</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear</button>
     
     </div>
      <div className="container my-3"  style={{color: props.mode ==='dark'?'white':'#042743'}}>
       <h2>Your text summary</h2>
       <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} and {text.length} characters </p>
-      <p>{0.008*text.split(" ").length} minutes read </p>
+      <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read </p>
       <h2>Preview</h2>
-      <p>{text.length>0?text:"Enter something above to preview here"}</p>
+      <p>{text.length>0?text:"Nothing to preview here."}</p>
      </div>
     </>
   )
